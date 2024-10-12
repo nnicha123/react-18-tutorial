acad89b79093b6b7098486b5026160638f66d6fe
nnicha123 <nicha_ngamtweerat@hotmail.com>
2024-10-12 14:49:31 +0700

    Learn about react hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,9 @@
-import ListGroup from "./components/ListGroup";
-import Alert from "./components/Alert";
-import Button from "./components/Button/Button";
-import { useState } from "react";
-import "./App.css";
-import { BsFillCalendarFill } from "react-icons/bs";
-import Like from "./components/Like";
+import Form from "./components/Form";
 
 function App() {
-  let items: any[] = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
-  const [alertVisible, setAlertVisibility] = useState(false);
-
   return (
     <div>
-      {/* <BsFillCalendarFill color="red" size={"25px"} />
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      /> */}
-      {/* {alertVisible && (
-        <Alert onClose={() => setAlertVisibility(false)}>
-          Hello <span>World</span>
-        </Alert>
-      )}
-      <Button color="primary" onClick={() => setAlertVisibility(true)}>
-        My Button
-      </Button> */}
-      <Like onClick={() => console.log("clicked")} />
+      <Form />
     </div>
   );
 }
--- src/Message.tsx
+++ src/Message.tsx
@@ -1,7 +1,11 @@
 function Message() {
-  const name = "Mosh";
-  if (name) return <h1>Hello World {name}</h1>;
-  return <h1>Hello World</h1>;
+  let count = 0;
+  count++;
+  console.log(count);
+  return <div>Message {count}</div>;
+  // const name = "Mosh";
+  // if (name) return <h1>Hello World {name}</h1>;
+  // return <h1>Hello World</h1>;
 }
 
 export default Message;
